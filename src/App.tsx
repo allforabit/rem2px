b7d904b9ff880cbebdcb804d07f119deedfb1c1a
allforabit <kevnolan@gmail.com>
2022-01-25 21:58:51 +0000

    react and prettier

---
+++ src/App.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+export function App() {
+  return <h1 className="bg-black p-3 text-white">Hello world!</h1>;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx,json}",
-  ],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx,json}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
