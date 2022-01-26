2eca6c1d7d797ea70961b2f7237065c8db9a9538
allforabit <kevnolan@gmail.com>
2022-01-26 20:52:11 +0000

    tw headless

--- src/background.ts
+++ src/background.ts
@@ -1,17 +1,19 @@
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: "#faa00a" }, function () {
-    console.log("The color is orange.");
-  });
+  chrome.storage.sync.set({ color: '#faa00a' }, function () {
+    console.log('The color is orange.')
+  })
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: "developer.chrome.com" },
+            pageUrl: {
+              schemes: ['http', 'https'],
+            },
           }),
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()],
       },
-    ]);
-  });
-});
+    ])
+  })
+})
